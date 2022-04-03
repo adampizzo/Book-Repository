9c5836cbfbfa2652c0233221b9ae30cc41f1a15c
adampizzo <Adampizzo@icloud.com>
2022-04-03 19:29:46 -0400

    firstcommit

---
+++ app.py
@@ -0,0 +1,8 @@
+# import models
+# main menu  - add, search, analysis, exit, view options
+# add books to the database
+# edit books
+# delete books
+# search books
+# data cleaning
+# loop runs program
