8dc5f27ab609bf4cf07421d52a66cfc638ae037d
adampizzo <Adampizzo@icloud.com>
2022-04-09 11:24:08 -0400

    Added suggest books
    wrote 3 functions
    clean_date - get the date in a proper Datetime form for the DB.
    clean_price - turned price into cents to help prevent data rounding errors with floats.
    add_csv - runs all of the commands to add to DB. Checks to see if the entry already exists.

--- app.py
+++ app.py
@@ -1,13 +1,87 @@
 # import models
 from models import (Base, session,
                     Book, engine)
-# main menu  - add, search, analysis, exit, view options
+import datetime
+import csv
+
 # add books to the database
 # edit books
 # delete books
 # search books
 # data cleaning
-# loop runs program
+
+def clean_date(date_str):
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 
+            'July', 'August', 'September', 'October', 'November', 'December']
+    split_date = date_str.split(' ')
+    month = int(months.index(split_date[0]) + 1)
+    day = int(split_date[1].split(',')[0])
+    year = int(split_date[2])
+    return datetime.date(year, month, day)
+
+
+def clean_price(price_str):
+    return int(float(price_str) * 100)
+
+
+def menu():
+    while True:
+        print('''
+            \nPROGRAMMING BOOKS
+            \r1) Add Book
+            \r2) View all books
+            \r3) Search for book
+            \r4) Book Analysis
+            \r5) Exit''')
+        choice = input('What would you like to do? ')
+        if choice in ['1', '2', '3', '4', '5']:
+            return choice
+        else:
+            input('''
+                \rPlease choose of the options above.
+                \rA number from 1-5.
+                \rPress enter to try again.''')
+            
+
+def add_csv():
+    with open('suggested_books.csv') as csvfile:
+        data = csv.reader(csvfile)
+        for row in data:
+            book_in_db = session.query(Book).filter(Book.title==row[0]).one_or_none()
+            if book_in_db == None:
+                title = row[0]
+                author = row[1]
+                date = clean_date(row[2])
+                price = clean_price(row[3])
+                new_book = Book(title=title, author=author, published_date=date, price=price)
+                session.add(new_book)
+            else:
+                print(f'"{book_in_db.title}" already exists in the database.')
+        session.commit()
+            
+
+
+def app():
+    app_running = True
+    while app_running:
+        choice = menu()
+        if choice == '1':
+            # add book
+            pass
+        elif choice == '2':
+            # view all books
+            pass
+        elif choice == '3':
+            # Search for book
+            pass
+        elif choice == '4':
+            # Book Analysis
+            pass
+        else:
+            print('GOODBYE')
+            app_running = False
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    # app()
+    add_csv()
